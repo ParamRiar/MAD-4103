ce92386f634fb7e159bbee4c6b038d9369f968c2
Param <32308577+ParamRiar@users.noreply.github.com>
2017-10-21 14:20:52 -0400

    Add files via upload

---
+++ www1/js/index.js
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+		alert ("hello");
+		
+		document.getElementById("create").addEventListener("click",createContact);
+		
+document.getElementById("findContact").addEventListener("click", findContact);
+document.getElementById("deleteContact").addEventListener("click", deleteContact);
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+function createContact(){
+	alert("create calling");
+	var myContact= navigator.contacts.create();
+	var phoneNumbers=[];
+	phoneNumbers[0] = new ContactField('work','123-6787-890',false);
+	phoneNumbers[1] = new ContactField('office','123-6787-890',false);
+	myContact.displayName = "Test User";
+	myContact.displayNumbers = phoneNumbers;
+	myContact.save(contactSuccess, contactError);
+	function contactSuccess() {
+      alert("Contact is saved!");
+   }
+	
+   function contactError(message) {
+      alert('Failed because: ' + message);
+   }
+	
+
+}
